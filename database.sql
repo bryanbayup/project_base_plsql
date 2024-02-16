3fcd015b0b58e7e7368f566e6ae2ae3ed0a7de2d
Briyan Bayu Pagestu <59904898+bryanbayup@users.noreply.github.com>
2024-02-16 22:58:22 +0800

    menambahkan create table log dan trigger

--- database.sql
+++ database.sql
@@ -3,3 +3,32 @@ CREATE TABLE user (
     username VARCHAR(50) UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
+
+-- Tambahkan tabel log
+CREATE TABLE user_log (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50),
+    action VARCHAR(10),
+    timestamp TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Tambahkan trigger pada tabel user
+CREATE OR REPLACE FUNCTION user_log_trigger()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO user_log (username, action) VALUES (NEW.username, 'INSERT');
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO user_log (username, action) VALUES (NEW.username, 'UPDATE');
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO user_log (username, action) VALUES (OLD.username, 'DELETE');
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER user_trigger
+AFTER INSERT OR UPDATE OR DELETE
+ON users
+FOR EACH ROW
+EXECUTE FUNCTION user_log_trigger();
